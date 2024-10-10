f661960523833e9274692c295a63d9bb97e3797c
Laiba Muhammad Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2024-10-10 16:44:50 +0500

    react via cdn

---
+++ index.js
@@ -0,0 +1,6 @@
+var h1 = React.createElement('h1', null, "Hello from React")
+
+var parent = document.querySelector("#parent")
+// console.log(parent)
+var root = ReactDOM.createRoot(parent) 
+root.render(h1)
