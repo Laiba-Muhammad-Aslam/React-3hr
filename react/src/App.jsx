7bdf1e4d66eb048e04853b9abe47d57906aa314b
Laiba Muhammad Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2024-10-18 19:27:14 +0500

    Learn how to handle a form and change its default behavior.

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,17 +1,34 @@
 import React from 'react'
 
-const App = () => {
-  let user = "Maha"
+// const App = () => {
+//   let user = "Maha"
 
-   const changeUser = () =>{
-    console.log(user)
-    user = "Soha"
-    console.log(user)
+//    const changeUser = () =>{
+//     console.log(user)
+//     user = "Soha"
+//     console.log(user)
+//   }
+//   return (
+//     <div>
+//       <h1>Username is {user}</h1>
+//       <button onClick={changeUser}>Change Name</button>
+//     </div>
+//   )
+// }
+
+const App = () => {
+  const formHandler = (e) => {
+    e.preventDefault()
+    console.log("Submitted")
   }
   return (
     <div>
-      <h1>Username is {user}</h1>
-      <button onClick={changeUser}>Change Name</button>
+      <form onSubmit={(e)=>{
+        formHandler(e)
+      }}>
+        <input type="text" placeholder='Enetr your name' />
+        <button>Submit</button>
+      </form>
     </div>
   )
 }
